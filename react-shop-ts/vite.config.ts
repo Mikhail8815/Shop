8aced239a96a70aaaaca5c582c0df35c4a2cb4d9
Mikhail8815 <mishutka150588@mail.ru>
2025-08-09 15:58:20 +0300

    ref: routes, config

--- react-shop-ts/vite.config.ts
+++ react-shop-ts/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-shop-ts/"
+  base: "/", // Изменили на корень
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
