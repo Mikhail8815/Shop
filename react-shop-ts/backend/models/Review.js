b10e5940a092c85d1aaa3dc0c13f1cd6e05bbe43
Mikhail8815 <mishutka150588@mail.ru>
2026-02-22 20:41:04 +0300

    add model for review

---
+++ react-shop-ts/backend/models/Review.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    productId: {
+        type: Number,
+        required: [true, 'ID товара обязателен'],
+        index: true
+    },
+
+    userName: {
+        type: String,
+        required: [true, 'Имя автора обязательно'],
+        trim: true
+    },
+
+    rating: {
+        type: Number,
+        required: [true, 'Оценка обязательна'],
+        min: [1, 'Минимальная оценка - 1'],
+        max: [5, 'Максимальная оценка - 5']
+    },
+
+    text: {
+        type: String,
+        required: [true, 'Текст отзыва обязателен'],
+        trim: true,
+        minlength: [3, 'Минимум 3 символа']
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
